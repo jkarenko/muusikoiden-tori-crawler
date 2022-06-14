cb28778cf1badcdb0807084aceaa09cb6100dd00
Juho Karenko <juho.karenko@gmail.com>
2022-06-15 01:10:47 +0300

    implement changing classified type
    sell and buy now supported

--- muusikoidenTori.py
+++ muusikoidenTori.py
@@ -5,24 +5,48 @@ from bs4 import BeautifulSoup
 
 BASE_URL = "https://muusikoiden.net/"
 SEARCH_URL = "/tori/haku.php?keyword="
+QUERY_PREFIX = "&type="
 
-def get_items_from_url(url):
+
+class Item:
+    def __init__(self, name, description, price, link):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.link = link
+
+    def __str__(self):
+        return f"{self.name} - {self.price} - {self.link}"
+
+
+def get_items_from_url(url, query_type):
+    keyword = "Myydään:" if query_type == "sell" else "Ostetaan:"
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
-    items = {}
-    names = [element.parent.text for element in soup.find_all("b", text=re.compile('Myydään:'))]
-    descriptions = [element.parent.parent.text for element in soup.find_all("b", text=re.compile('Myydään:'))]
+    items_list = []
+    names = [element.parent.text for element in soup.find_all("b", text=re.compile(keyword))]
+    descriptions = [element.parent.parent.text for element in soup.find_all("b", text=re.compile(keyword))]
     links = [BASE_URL + element['href'] for element in
-             soup.find_all("a", href=re.compile(r'/tori/ilmoitus/\d+$'))]
-    prices = [element.parent.text for element in soup.find_all("b", text=re.compile('Hinta:'))]
+             soup.find_all("a", href=re.compile(r'^/tori/ilmoitus/\d+$'))]
+    if query_type == "sell":
+        prices = [element.parent.text for element in soup.find_all("b", text=re.compile('Hinta:'))]
+    else:
+        # make an empty list as long as names
+        prices = [None] * len(names)
     for (name, description, price, link) in zip(names, descriptions, prices, links):
-        items[name] = {'description': description, 'price': price, 'link': link}
-    return items
+        items_list.append(Item(name, description, price, link))
+    return items_list
+
+
+def search(query, query_type):
+    url = BASE_URL + SEARCH_URL + query + QUERY_PREFIX + query_type
+    return get_items_from_url(url, query_type)
 
 
 if __name__ == "__main__":
-    search_term = "sm58"
-    items = get_items_from_url(BASE_URL + SEARCH_URL + search_term)
-    for name, data in items.items():
-        description, price, link = data['description'], data['price'], data['link']
-        print(name, price, link)
+    search_term = "ibanez"
+    query_type = "buy"
+    url = BASE_URL + SEARCH_URL + search_term + QUERY_PREFIX + query_type
+    items = get_items_from_url(url, query_type)
+    for item in items:
+        print(item)
